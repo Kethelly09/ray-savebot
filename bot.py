cf18188c8200fcd5db70311f797ac43d9b772ce0
Kethelly09 <silvarayana158@gmail.com>
2026-02-25 14:11:57 -0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -7,7 +7,7 @@ TOKEN = os.getenv("TOKEN")
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text(
-        "Ola Me envie o link do vídeo e eu o baixarei para você.  🎬"
+        "Ola Me envie o link do vídeo e eu o baixarei para você.🎬"
     )
 
 def main():
