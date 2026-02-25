37f63fa372dbee6505ca7eb3588af72cea0fcc76
Kethelly09 <silvarayana158@gmail.com>
2026-02-25 00:24:12 -0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,1 +1,26 @@
 
+import os
+from telegram import Update
+from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
+
+TOKEN = os.getenv("TOKEN")
+
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text(
+        "Send me a video link and I will download it for you 🎬"
+    )
+
+def main():
+    if not TOKEN:
+        print("TOKEN não encontrado!")
+        return
+
+    print("BOT INICIANDO...")
+    app = ApplicationBuilder().token(TOKEN).build()
+    app.add_handler(CommandHandler("start", start))
+
+    print("BOT RODANDO...")
+    app.run_polling()
+
+if __name__ == "__main__":
+    main()
